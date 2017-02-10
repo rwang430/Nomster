88fd96cd523d9bbe2e4677a3513e8dd385d47fb4
Roger Wang <blumoo@gmail.com>
2017-02-10 11:12:47 +0000

    made changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
   def create
-    @place = Place.find(params[:place_id])
+    place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
@@ -9,6 +9,6 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209151839) do
+ActiveRecord::Schema.define(version: 20170210090729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,8 @@ ActiveRecord::Schema.define(version: 20170209151839) do
   end
 
   create_table "photos", force: true do |t|
-    t.string   "photos"
+    t.text     "caption"
+    t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
