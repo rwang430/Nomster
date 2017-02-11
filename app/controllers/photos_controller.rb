55ead92e16639e2faae804676be3d366feb095f9
Roger Wang <blumoo@gmail.com>
2017-02-11 08:39:44 +0000

    edit changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,14 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
   def create
-    place = Place.find(params[:place_id])
-    @photo = place.photos.create(photo_params)
-    redirect_to place_path(place)
+    @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params)
+    redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:caption, :picture1800)
+    params.require(:photo).permit(:caption, :picture)
   end
 end
