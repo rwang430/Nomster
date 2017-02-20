9370df4c8ab16280683755c2d015c242011b8e50
Roger Wang <blumoo@gmail.com>
2017-02-20 01:05:01 +0000

    send email on comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,21 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+ # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+ # config.action_mailer.smtp_settings = {
+ #   address: "smtp.gmail.com",
+ #   port: 587,
+ #   domain: "example.com",
+ #   authentication: "plain",
+ #  enable_starttls_auto: true,
+ #   user_name: ENV['GMAIL_ADDRESS'],
+ #   password: ENV['GMAIL_PASSWORD']
+ # }
 
+  # Return mailer to use test mode, so instead of actually sending emails
+  # it will make a note in our log files.
+  config.action_mailer.delivery_method = :test
 
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
 
   # Settings specified here will take precedence over those in config/application.rb.
